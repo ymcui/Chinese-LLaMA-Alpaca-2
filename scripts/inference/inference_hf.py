150e751cf902671d43103f08e043bb7735243765
GoGoJoestar <qazxc59419@163.com>
2023-08-01 15:34:01 +0800

    add vLLM support

--- scripts/inference/inference_hf.py
+++ scripts/inference/inference_hf.py
@@ -12,8 +12,16 @@ parser.add_argument('--gpus', default="0", type=str)
 parser.add_argument('--only_cpu',action='store_true',help='only use CPU for inference')
 parser.add_argument('--alpha',type=str,default="1.0", help="The scaling factor of NTK method, can be a float or 'auto'. ")
 parser.add_argument('--load_in_8bit',action='store_true', help="Load the LLM in the 8bit mode")
+parser.add_argument("--use_vllm", action='store_true', help="Use vLLM as back-end LLM service.")
 
 args = parser.parse_args()
+if args.use_vllm:
+    if args.lora_model is not None:
+        raise ValueError("vLLM currently does not support LoRA, please merge the LoRA weights to the base model.")
+    if args.load_in_8bit:
+        raise ValueError("vLLM currently does not support quantization, please use fp16 (default) or unuse --use_vllm.")
+    if args.only_cpu:
+        raise ValueError("vLLM requires GPUs with compute capability not less than 7.0. If you want to run only on CPU, please unuse --use_vllm.")
 if args.only_cpu is True:
     args.gpus = ""
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
@@ -21,6 +29,8 @@ import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
 from transformers import GenerationConfig
 from peft import  PeftModel
+if args.use_vllm:
+    from vllm import LLM, SamplingParams
 
 import sys
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,15 +39,24 @@ from attn_and_long_ctx_patches import apply_attention_patch, apply_ntk_scaling_p
 apply_attention_patch(use_memory_efficient_attention=True)
 apply_ntk_scaling_patch(args.alpha)
 
-generation_config = GenerationConfig(
-    temperature=0.2,
-    top_k=40,
-    top_p=0.9,
-    do_sample=True,
-    num_beams=1,
-    repetition_penalty=1.1,
-    max_new_tokens=400
-)
+if args.use_vllm:
+    generation_config = dict(
+        temperature=0.2,
+        top_k=40,
+        top_p=0.9,
+        max_tokens=400,
+        presence_penalty=1.0,
+    )
+else:
+    generation_config = GenerationConfig(
+        temperature=0.2,
+        top_k=40,
+        top_p=0.9,
+        do_sample=True,
+        num_beams=1,
+        repetition_penalty=1.1,
+        max_new_tokens=400
+    )
 
 DEFAULT_SYSTEM_PROMPT = """You are a helpful assistant. 你是一个乐于助人的助手。"""
 
@@ -63,31 +82,41 @@ if __name__ == '__main__':
         args.tokenizer_path = args.lora_model
         if args.lora_model is None:
             args.tokenizer_path = args.base_model
-    tokenizer = LlamaTokenizer.from_pretrained(args.tokenizer_path, legacy=True)
 
-    base_model = LlamaForCausalLM.from_pretrained(
-        args.base_model,
-        load_in_8bit=args.load_in_8bit,
-        torch_dtype=load_type,
-        low_cpu_mem_usage=True,
-        device_map='auto',
-        )
-
-    model_vocab_size = base_model.get_input_embeddings().weight.size(0)
-    tokenzier_vocab_size = len(tokenizer)
-    print(f"Vocab of the base model: {model_vocab_size}")
-    print(f"Vocab of the tokenizer: {tokenzier_vocab_size}")
-    if model_vocab_size!=tokenzier_vocab_size:
-        print("Resize model embeddings to fit tokenizer")
-        base_model.resize_token_embeddings(tokenzier_vocab_size)
-    if args.lora_model is not None:
-        print("loading peft model")
-        model = PeftModel.from_pretrained(base_model, args.lora_model,torch_dtype=load_type,device_map='auto',)
+    if args.use_vllm:
+        model = LLM(model=args.base_model,
+            tokenizer=args.tokenizer_path,
+            tokenizer_mode='slow',
+            tensor_parallel_size=len(args.gpus.split(',')))
+        tokenizer = LlamaTokenizer.from_pretrained(args.tokenizer_path, legacy=True)
     else:
-        model = base_model
+        tokenizer = LlamaTokenizer.from_pretrained(args.tokenizer_path, legacy=True)
 
-    if device==torch.device('cpu'):
-        model.float()
+        base_model = LlamaForCausalLM.from_pretrained(
+            args.base_model,
+            load_in_8bit=args.load_in_8bit,
+            torch_dtype=load_type,
+            low_cpu_mem_usage=True,
+            device_map='auto',
+            )
+
+        model_vocab_size = base_model.get_input_embeddings().weight.size(0)
+        tokenzier_vocab_size = len(tokenizer)
+        print(f"Vocab of the base model: {model_vocab_size}")
+        print(f"Vocab of the tokenizer: {tokenzier_vocab_size}")
+        if model_vocab_size!=tokenzier_vocab_size:
+            print("Resize model embeddings to fit tokenizer")
+            base_model.resize_token_embeddings(tokenzier_vocab_size)
+        if args.lora_model is not None:
+            print("loading peft model")
+            model = PeftModel.from_pretrained(base_model, args.lora_model,torch_dtype=load_type,device_map='auto',)
+        else:
+            model = base_model
+
+        if device==torch.device('cpu'):
+            model.float()
+        model.eval()
+    
     # test data
     if args.data_file is None:
         examples = sample_data
@@ -97,7 +126,6 @@ if __name__ == '__main__':
         print("first 10 examples:")
         for example in examples[:10]:
             print(example)
-    model.eval()
 
     with torch.no_grad():
         if args.interactive:
@@ -119,52 +147,78 @@ if __name__ == '__main__':
                     input_text = generate_prompt(instruction=raw_input_text)
                 else:
                     input_text = raw_input_text
-                inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
-                generation_output = model.generate(
-                    input_ids = inputs["input_ids"].to(device),
-                    attention_mask = inputs['attention_mask'].to(device),
-                    eos_token_id=tokenizer.eos_token_id,
-                    pad_token_id=tokenizer.pad_token_id,
-                    generation_config = generation_config
-                )
-                s = generation_output[0]
-                output = tokenizer.decode(s,skip_special_tokens=True)
-                if args.with_prompt:
-                    response = output.split("[/INST]")[-1].strip()
+                
+                if args.use_vllm:
+                    output = model.generate([input_text], SamplingParams(**generation_config), use_tqdm=False)
+                    response = output[0].outputs[0].text
                 else:
-                    response = output
+                    inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
+                    generation_output = model.generate(
+                        input_ids = inputs["input_ids"].to(device),
+                        attention_mask = inputs['attention_mask'].to(device),
+                        eos_token_id=tokenizer.eos_token_id,
+                        pad_token_id=tokenizer.pad_token_id,
+                        generation_config = generation_config
+                    )
+                    s = generation_output[0]
+                    output = tokenizer.decode(s,skip_special_tokens=True)
+                    if args.with_prompt:
+                        response = output.split("[/INST]")[-1].strip()
+                    else:
+                        response = output
                 print("Response: ",response)
                 print("\n")
         else:
             print("Start inference.")
             results = []
-            for index, example in enumerate(examples):
+            if args.use_vllm:
                 if args.with_prompt is True:
-                    input_text = generate_prompt(instruction=example)
+                    inputs = [generate_prompt(example) for example in examples]
                 else:
-                    input_text = example
-                inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
-                generation_output = model.generate(
-                    input_ids = inputs["input_ids"].to(device),
-                    attention_mask = inputs['attention_mask'].to(device),
-                    eos_token_id=tokenizer.eos_token_id,
-                    pad_token_id=tokenizer.pad_token_id,
-                    generation_config = generation_config
-                )
-                s = generation_output[0]
-                output = tokenizer.decode(s,skip_special_tokens=True)
-                if args.with_prompt:
-                    response = output.split("[/INST]")[1].strip()
-                else:
-                    response = output
-                print(f"======={index}=======")
-                print(f"Input: {example}\n")
-                print(f"Output: {response}\n")
+                    inputs = examples
+                outputs = model.generate(inputs, SamplingParams(**generation_config))
 
-                results.append({"Input":input_text,"Output":response})
+                for index, (example, output) in enumerate(zip(examples, outputs)):
+                    response = output.outputs[0].text
+
+                    print(f"======={index}=======")
+                    print(f"Input: {example}\n")
+                    print(f"Output: {response}\n")
+
+                    results.append({"Input":example,"Output":response})
+            
+            else:        
+                for index, example in enumerate(examples):
+                    if args.with_prompt is True:
+                        input_text = generate_prompt(instruction=example)
+                    else:
+                        input_text = example
+                    inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
+                    generation_output = model.generate(
+                        input_ids = inputs["input_ids"].to(device),
+                        attention_mask = inputs['attention_mask'].to(device),
+                        eos_token_id=tokenizer.eos_token_id,
+                        pad_token_id=tokenizer.pad_token_id,
+                        generation_config = generation_config
+                    )
+                    s = generation_output[0]
+                    output = tokenizer.decode(s,skip_special_tokens=True)
+                    if args.with_prompt:
+                        response = output.split("[/INST]")[1].strip()
+                    else:
+                        response = output
+                    print(f"======={index}=======")
+                    print(f"Input: {example}\n")
+                    print(f"Output: {response}\n")
+
+                    results.append({"Input":input_text,"Output":response})
 
             dirname = os.path.dirname(args.predictions_file)
             os.makedirs(dirname,exist_ok=True)
             with open(args.predictions_file,'w') as f:
                 json.dump(results,f,ensure_ascii=False,indent=2)
-            generation_config.save_pretrained('./')
+            if args.use_vllm:
+                with open(dirname+'/generation_config.json','w') as f:
+                    json.dump(generation_config,f,ensure_ascii=False,indent=2)
+            else:
+                generation_config.save_pretrained('./')
