f090e7afa1734271a71207c6b9967e24e6b3ba37
Ziqing Yang <yangziqing@163.com>
2023-08-04 19:22:25 +0800

    Update langchain_qa.py

--- scripts/langchain/langchain_qa.py
+++ scripts/langchain/langchain_qa.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 parser = argparse.ArgumentParser()
-parser.add_argument('--file_path',required=True,type=str)
-parser.add_argument('--embedding_path',required=True,type=str)
-parser.add_argument('--model_path',required=True,type=str)
+parser.add_argument('--file_path', required=True, type=str)
+parser.add_argument('--embedding_path', required=True, type=str)
+parser.add_argument('--model_path', required=True, type=str)
 parser.add_argument('--gpu_id', default="0", type=str)
 parser.add_argument('--chain_type', default="refine", type=str)
 args = parser.parse_args()
